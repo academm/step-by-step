6ce54a9743180b2d3cd8496accc76ecc8391f01a
saa <saa@finextechnologies.com>
2019-07-28 20:17:20 +0300

    step 10

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+        "net/http"
 	"github.com/Sirupsen/logrus"
 	"github.com/takama/router"
+        "os"
+	common_handlers "github.com/academm/handlers"
+	"github.com/academm/step-by-step/version"
 )
 
 var log = logrus.New()
@@ -10,8 +14,21 @@ var log = logrus.New()
 // Run server: go build && step-by-step
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
+        port := os.Getenv("SERVICE_PORT")
+	if len(port) == 0 {
+		log.Fatal("Required parameter service port is not set")
+	}
 	r := router.New()
 	r.Logger = logger
 	r.GET("/", home)
-	r.Listen(":8000")
+
+	// Readiness and liveness probes for Kubernetes
+	r.GET("/info", func(c *router.Control) {
+		common_handlers.Info(c, version.RELEASE, version.REPO, version.COMMIT)
+	})
+	r.GET("/healthz", func(c *router.Control) {
+		c.Code(http.StatusOK).Body(http.StatusText(http.StatusOK))
+	})
+
+        r.Listen("0.0.0.0:" + port)
 }
